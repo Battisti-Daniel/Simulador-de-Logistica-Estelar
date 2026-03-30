ce116df680c558ee391d9ad93e83ae5853826b88
Battisti-Daniel <danielbattisti@outlook.com.br>
2026-03-30 17:10:29 -0300

    chore: stop tracking generated build artifacts

--- build/CMakeFiles/trabalh_m1.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for trabalh_m1.
-# This may be replaced when dependencies are built.
--- build/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/danie/OneDrive/Desktop/test/Simulador-de-Logistica-Estelar/build/_deps/raylib-src/src
-# Build directory: C:/Users/danie/OneDrive/Desktop/test/Simulador-de-Logistica-Estelar/build/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
